b0c7bd4be1f37b66f4490bcfb8b69e9986e24f96
tv0823 <tv0823@bs.amalnet.k12.il>
2024-04-06 19:23:14 +0300

    second activity --> calculate the roots and show them

--- app/src/main/java/com/example/quadraticformulasolver/MainActivity.java
+++ app/src/main/java/com/example/quadraticformulasolver/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -16,8 +17,10 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
     EditText a, b, c;
     Intent si;
+    TextView showRootAns;
     String aStr, bStr, cStr;
-    Random rnd = new Random();;
+    Random rnd = new Random();
+    ;
     final int REQUEST_CODE = 3602;
 
 
@@ -30,13 +33,15 @@ public class MainActivity extends AppCompatActivity {
         b = findViewById(R.id.b);
         c = findViewById(R.id.c);
 
-        si = new Intent(this,SolutionActivity.class);
+        showRootAns = findViewById(R.id.showRootAns);
+
+        si = new Intent(this, SolutionActivity.class);
     }
 
     public void randomNums(View view) {
-        a.setText("" + (rnd.nextInt(1000 + 1000)-1000));
-        b.setText("" + (rnd.nextInt(1000 + 1000)-1000));
-        c.setText("" + (rnd.nextInt(1000 + 1000)-1000));
+        a.setText("" + (rnd.nextInt(1000 + 1000) - 1000));
+        b.setText("" + (rnd.nextInt(1000 + 1000) - 1000));
+        c.setText("" + (rnd.nextInt(1000 + 1000) - 1000));
     }
 
     public void getValues(View view) {
@@ -46,15 +51,21 @@ public class MainActivity extends AppCompatActivity {
 
         if (aStr.isEmpty() || bStr.isEmpty() || cStr.isEmpty() || aStr.equals("-") || cStr.equals("-") || bStr.equals("-") || aStr.equals(".") || bStr.equals(".") || cStr.equals(".") || aStr.equals("-.") || cStr.equals("-.") || bStr.equals("-.")) {
             Toast.makeText(this, "Invalid input", Toast.LENGTH_SHORT).show();
-        }
-        else if(Double.parseDouble(aStr) == 0.0) {
+        } else if (Double.parseDouble(aStr) == 0.0) {
             Toast.makeText(this, "a cannot be equal to 0", Toast.LENGTH_SHORT).show();
-        }
-        else {
+        } else {
             si.putExtra("a", Double.parseDouble(aStr));
             si.putExtra("b", Double.parseDouble(bStr));
             si.putExtra("c", Double.parseDouble(cStr));
             startActivityForResult(si, REQUEST_CODE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int source, int result, Intent data_back) {
+        super.onActivityResult(source, result, data_back);
+        if (data_back != null) {
+            showRootAns.setText("The answers are:\n" + data_back.getStringExtra("ans"));
         }
     }
 }
--- app/src/main/java/com/example/quadraticformulasolver/SolutionActivity.java
+++ app/src/main/java/com/example/quadraticformulasolver/SolutionActivity.java
@@ -2,23 +2,72 @@ package com.example.quadraticformulasolver;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import android.content.Intent;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
+
 
 public class SolutionActivity extends AppCompatActivity {
+    Intent gi;
+    TextView showAns;
+    WebView wV;
+    double a, b, c, discriminant, root1, root2;
+    String ans;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_solution);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        showAns = findViewById(R.id.showAns);
+        wV =findViewById(R.id.wV);
+
+        wV.getSettings().setJavaScriptEnabled(true);
+        wV.setWebViewClient(new MyWebViewClient());
+
+        gi = getIntent();
+        a = gi.getDoubleExtra("a",1);
+        b = gi.getDoubleExtra("b",1);
+        c = gi.getDoubleExtra("c",1);
+
+        solveEquation();
+        createAns();
+
+        showAns.setText(ans);
+        wV.loadUrl("https://www.google.com/search?q=" + a + "x%5E2%2B" + b + "*x%2B" + c);
+
+    }
+
+    public void solveEquation(){
+        discriminant = b*b - 4*a*c;
+        root1 = (-b + Math.sqrt(discriminant))/(2*a);
+        root2 = (-b - Math.sqrt(discriminant))/(2*a);
+    }
+
+    public void createAns(){
+        if(discriminant > 0) {
+            ans = "X1= " + root1 + "\nX2= " + root2;
+        } else if (discriminant == 0) {
+            ans = "X1= " + root1;
+        } else{
+            ans = "There is no solution for this equation";
+        }
+    }
+
+    public void goBack(View view) {
+        gi.putExtra("ans", ans);
+        setResult(RESULT_OK, gi);
+        finish();
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+        public boolean shouldOverideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
     }
 }
